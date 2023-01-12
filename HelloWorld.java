50a5dc0e045396b56acb4fec709b73d910c69543
Nagaraj Naik <mn.naik459@gmail.com>
2023-01-12 02:23:05 +0000

    added sankranthi

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Good evening guys!"); 
+        System.out.println("Hello, Good evening guys!");
+        System.out.println("Happy Pongal");
+        System.out.println("Happy Makara Sankranthi");
+	System.out.println("Happy Kanuma");
+	
     }
 }
